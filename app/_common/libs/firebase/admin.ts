c13f523c18898fa871f3cf20a99030fe675dfe57
github takaryu39 <ryuta0takamiya@gmail.com>
2023-12-02 18:03:24 +0900

    firebaseのSDK初期化の方法を変更

--- app/_common/libs/firebase/admin.ts
+++ app/_common/libs/firebase/admin.ts
@@ -5,7 +5,11 @@ const serviceAccount = require('@/firebaseSecretKey.json')
 export const firebaseAdmin =
   getApps()[0] ??
   initializeApp({
-    credential: cert(serviceAccount),
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    }),
   })
 
 export const auth = getAuth()
