7dcd9ba50b08d917a50a5a325af3ba294a358382
github takaryu39 <ryuta0takamiya@gmail.com>
2023-12-02 18:09:48 +0900

    デプロイ時のエラー修正

--- app/_common/libs/firebase/admin.ts
+++ app/_common/libs/firebase/admin.ts
@@ -1,7 +1,6 @@
 import { initializeApp, cert, getApps } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
-const serviceAccount = require('@/firebaseSecretKey.json')
 export const firebaseAdmin =
   getApps()[0] ??
   initializeApp({
